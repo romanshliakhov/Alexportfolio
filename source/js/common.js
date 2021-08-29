1fc2830dc3e79961bdb666d382a82571935e2af4
romanshliakhov <romanshliakhov@gmail.com>
2021-08-29 23:21:48 +0300

    added about section

---
+++ source/js/common.js
@@ -0,0 +1,14 @@
+// menu
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeMenu = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+
